15400c37e74ae151021d1be623109b030218ccc0
4uy <64391250+4uy@users.noreply.github.com>
2022-01-29 20:21:39 -0600

    Add basic web socket server for testing with CC

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+import { WebSocketServer, WebSocketClient } from "https://deno.land/x/websocket/mod.ts";
+
+const wss = new WebSocketServer(1337);
+
+wss.on("connection", (ws: WebSocketClient) => {
+  console.log("New connection.");
+});
